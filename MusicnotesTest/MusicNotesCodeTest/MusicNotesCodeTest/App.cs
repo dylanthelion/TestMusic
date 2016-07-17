ffe9fa25bdf2aea6001c25007f62c9fd883d1267
dylanthelion <dillion256@gmail.com>
2016-07-16 23:47:36 -0500

    Initial

---
+++ MusicnotesTest/MusicNotesCodeTest/MusicNotesCodeTest/App.cs
@@ -0,0 +1,28 @@
+﻿using MusicNotesCodeTest.code.view;
+using Xamarin.Forms;
+
+namespace MusicNotesCodeTest
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new MainMasterDetailPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MusicnotesTest/MusicNotesCodeTest/MusicNotesCodeTest/code/view/Library/LibraryPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MusicNotesCodeTest.code.view.Library
+{
+    public partial class LibraryPage : ContentPage
+    {
+        public LibraryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
