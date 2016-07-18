03a47d5ba285a613c1f6eba9c65a309a7fe60127
dylanthelion <dillion256@gmail.com>
2016-07-18 00:16:31 -0500

    Model Halfway done. Got class built in root project, was able to run app on my ipad.

---
+++ MusicnotesTest/MusicNotesCodeTest/MusicNotesCodeTest/code/model/Song/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicNotesCodeTest.code.model
+{
+    class Song
+    {
+        public string id { get; set; }
+        public string artist { get; set; }
+        public string title { get; set; }
+        public string key { get; set; }
+        public string instruments { get; set; }
+        public int pages { get; set; }
+    }
+}
