4780046822c65b14ded31c29adc13b9f71bec329
dylanthelion <dillion256@gmail.com>
2016-07-18 22:54:15 -0500

    Task 1 is finished. Will require layout tweaking.

--- MusicnotesTest/MusicNotesCodeTest/MusicNotesCodeTest/code/model/Song/Song.cs
+++ MusicnotesTest/MusicNotesCodeTest/MusicNotesCodeTest/code/model/Song/Song.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MusicNotesCodeTest.code.model
 {
-    class Song
+    class SongModel
     {
         public string id { get; set; }
         public string artist { get; set; }
@@ -14,5 +14,13 @@ namespace MusicNotesCodeTest.code.model
         public string key { get; set; }
         public string instruments { get; set; }
         public int pages { get; set; }
+        public int views { get; set; }
+        public string longDescription
+        {
+            get
+            {
+                return String.Format("Artist: {0} | View Count: {1} | Pages: {2}", this.artist, this.views.ToString(), this.pages);
+            }
+        }
     }
 }
--- MusicnotesTest/MusicNotesCodeTest/MusicNotesCodeTest/code/view/Library/LibraryPage.xaml.cs
+++ MusicnotesTest/MusicNotesCodeTest/MusicNotesCodeTest/code/view/Library/LibraryPage.xaml.cs
@@ -1,18 +1,71 @@
-﻿using System;
+﻿using MusicNotesCodeTest.code.model;
+using Org.XmlPull.V1;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Windows.Data.Xml.Dom;
 using Xamarin.Forms;
 
 namespace MusicNotesCodeTest.code.view.Library
 {
     public partial class LibraryPage : ContentPage
     {
+        ObservableCollection<SongModel> songs = new ObservableCollection<SongModel>();
+        //public string[] songs;
         public LibraryPage()
         {
             InitializeComponent();
+            loadSongs();
+            loadSongsFromFile();
+            songList.ItemsSource = songs;
+            Debug.WriteLine("Populated");
+        }
+
+        void OnSelection (object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem == null)
+            {
+                return;
+            }
+            Debug.WriteLine("HERP");
+            DisplayAlert("Select: ", e.SelectedItem.ToString(), "OK");
+        }
+
+        private void loadSongs()
+        {
+            SongModel s = new SongModel();
+            s.title = "Song One";
+            SongModel s2 = new SongModel();
+            s2.title = "Song Two";
+            songs = new ObservableCollection<SongModel>() { s, s2 };
+            Debug.WriteLine("Songs loaded");
+        }
+
+        private void loadSongsFromFile()
+        {
+            XDocument songsFromFile = XDocument.Load("songs.xml");
+            var query = songsFromFile.Descendants("songs").Descendants("song").Select(x => new SongModel
+            {
+                id = (string)x.Element("id"),
+                title = (string)x.Element("title"),
+                artist = (string)x.Element("artist"),
+                key = (string)x.Element("key"),
+                instruments = (string)x.Element("instruments"),
+                pages = (int)x.Element("pages"),
+                views = 0
+            }).ToList();
+            foreach(SongModel s in query)
+            {
+                songs.Add(s);
+            }
         }
     }
 }
